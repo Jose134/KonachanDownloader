3f24cf5647651d8b0750b1af3b35773b31213ea9
DarkBird <darkbird@vivaldi.net>
2020-03-24 17:16:24 +0100

    Moving to xml because I can get the total amount of posts with that tag

--- popup/page.js
+++ popup/page.js
@@ -1,21 +1,39 @@
 function onUpdatedTab (tabId, changeInfo, tabInfo) {
-    console.log(changeInfo.status);
     if (changeInfo.status != "complete") {
         return;
     }
 
+    /*
     browser.tabs.executeScript(
         tabId,
-        { code: "var result = document.body.innerText; document.body.innerHTML = ''; result" }
+        { code: "console.log(input.jsonPretty.textContent);"}
+    ).then(function (response) {
+        console.log(response);
+    });
+    */
+
+    browser.tabs.executeScript(
+        tabId,
+        { code: "var object = document.getElementsByTagName('posts')[0]; var result = { count: object.attributes.count.value, text: object.innerHTML }; object.innerHTML=''; document.body=document.createElement('body'); result" }
     ).then(function (response) {
         browser.tabs.insertCSS(tabId, {
             file: "preview.css"
         });
+        var count = response[0].count;
+        var text = response[0].text;
+        text = "<posts>" + text + "</posts>";
 
-        var data = JSON.parse(response);
-        
-        data.forEach(element => {
+        var json = xmlToJson($.parseXML(text));
+        json = json.posts.post;
+
+        console.log(response[0].count);
+        console.log(json);
+
+        json.forEach(element => {
+            element = element['@attributes'];
+            console.log(element);
             var preview = element.preview_url;
+            
             browser.tabs.executeScript(
                 tabId,
                 { code: "document.body.innerHTML += '<div class=\"previewdiv\"><img class = \"previewimg\" src=\"" + preview + "\"></img></div>';"}
@@ -30,7 +48,7 @@ function onUpdatedTab (tabId, changeInfo, tabInfo) {
         });
     });
 
-    //browser.tabs.onUpdated.removeListener(onUpdatedTab);
+    browser.tabs.onUpdated.removeListener(onUpdatedTab);
 }
 
 var tags;
@@ -46,7 +64,8 @@ function owo (e) {
     tags = tags.split(' ').join('_');
     tags = tags.split('+').join('%2B');
 
-    var url = "https://onewaifuaday.000webhostapp.com/getkonachan.php?tags=" + tags;
+    //var url = "https://onewaifuaday.000webhostapp.com/getkonachan.php?tags=" + tags;
+    var url = "https://konachan.com/post.xml?tags=" + tags;
 
     browser.tabs.onUpdated.addListener(onUpdatedTab);
     var creating = browser.tabs.create({
